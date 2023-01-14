cc68bb7c7114fc2c0e8e2e8d6060c8234b97af8e
Andrei Tava <tavaandrei@gmail.com>
2023-01-14 18:52:43 +0200

    some more scaffolding and boilerplate

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PricePipe } from './core/pipes/price.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PricePipe
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/data/game.ts
@@ -0,0 +1,7 @@
+export interface Game {
+  id: string,
+  name: string,
+  developer: string,
+  price: number,
+  releaseDate: Date
+}
