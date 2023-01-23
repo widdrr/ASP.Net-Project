86e4ee38190c89b214e8f42a1013c55432fdc39f
Andrei Tava <tavaandrei@gmail.com>
2023-01-23 11:26:41 +0200

    made the login screen pretty

--- Frontend/src/app/pages/auth/auth.module.ts
+++ Frontend/src/app/pages/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 
 
 
@@ -11,7 +14,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     LoginComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
+
   ]
 })
 export class AuthModule { }
